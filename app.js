a8a27788bb2bb1ac1c9b96ad33a5e9477b7022e7
randyluna222 <108312411+randyluna222@users.noreply.github.com>
2023-05-31 22:01:44 -0600

    Add modules

--- app.js
+++ app.js
@@ -1,62 +1,22 @@
-function getJoke(url) {
-  return fetch(url, {
-    headers: {
-      'Accept': 'application/json'
-    }
-  })
-  .then(response => {
-    if (!response.ok) {
-      throw new Error(`¡Error HTTP! Estado: ${response.status}`);
-    }
-    return response.json();
-  })
-  .then(data => {
-    return data.joke;
-  })
-  .catch(error => {
-    console.error(error);
-  });
-}
+import { searchJoke, displayJokes } from "./apisearch.js";
+import { getJoke } from "./api.js";
 
 // Random Joke
-const btnJoke = document.getElementById('randomJoke');
-const jokeText = document.getElementById('jokesGenerator');
+export const btnJoke = document.getElementById("randomJoke");
+export const resultContainer = document.getElementById("jokesGenerator1");
 
-btnJoke.addEventListener('click', () => {
-  getJoke('https://icanhazdadjoke.com/')
-    .then(joke => {
-      jokeText.textContent = joke;
-    });
-});
+const jokeElement = document.createElement("p");
 
-// Buscar chistes
-function searchJoke() {
-  const searchInput = document.getElementById('search').value;
-  const apiUrl = 'https://icanhazdadjoke.com/search?term=' + encodeURIComponent(searchInput);
+btnJoke.addEventListener("click", () => {
+  getJoke("https://icanhazdadjoke.com/").then((joke) => {
+    const jokeText = document.createElement("a");
+    jokeText.href = "./product.html"; 
 
-  return fetch(apiUrl, {
-    headers: {
-      'Accept': 'application/json'
-    }
-  })
-  .then(response => response.json())
-  .then(data => displayJokes(data.results))
-  .catch(error => console.log(error));
-}
-
-// Chistes encontrados
-function displayJokes(jokes) {
-  const resultContainer = document.getElementById('jokesGenerator');
-  resultContainer.innerHTML = '';
-
-  if (jokes.length === 0) {
-    resultContainer.innerHTML = 'No se encontraron chistes';
-    return;
-  }
-
-  jokes.forEach(joke => {
-    const jokeElement = document.createElement('p');
-    jokeElement.textContent = joke.joke; 
+    jokeText.textContent = joke;
+    jokeElement.appendChild(jokeText);
     resultContainer.appendChild(jokeElement);
   });
-}
+});
+
+searchJoke();
+displayJokes();
---
+++ changeimg.js
@@ -0,0 +1,49 @@
+export const products = {
+  shirt: [
+    {
+      price: '$13',
+      color: 'black'
+    },
+    {
+      price: '$10',
+      color: 'white'
+    }
+  ],
+  case: [
+    {
+      price: '$7',
+      color: 'black'
+    },
+    {
+      price: '$5',
+      color: 'white'
+    }
+  ],
+  poster: [
+    {
+      price: '$5',
+      color: 'black'
+    },
+    {
+      price: '$3',
+      color: 'white'
+    }
+  ],
+  almohada: [
+    {
+      price: '$15',
+      color: 'black'
+    },
+    {
+      price: '$12',
+      color: 'white'
+    }
+  ]
+};
+
+
+
+
+
+
+
